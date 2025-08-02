7d35d64fd97de7219a91273dd798176a0fab954a
Wesley Evans <162752827+evans-rxfire@users.noreply.github.com>
2025-08-01 21:43:52 -0500

    2025.08.01 Work on link buttons and styling.

---
+++ js/draft.js
@@ -0,0 +1,6 @@
+// import { fetchUser, fetchAllLeagues, fetchSingleLeague, fetchAllDrafts, fetchSingleDraft, fetchRosters, fetchPlayers } from "js/sleeperApi.js";
+// import {  } from "js/constants.js";
+
+let userData = {};
+let leagueData = {};
+
